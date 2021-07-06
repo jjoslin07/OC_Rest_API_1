b2e57d618334c687fa3133e87023358477b85bac
Joseph Joslin <jjoslin07@gmail.com>
2021-07-06 12:36:50 -0700

    Send back stuff for sale

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const app = express();
 
-// Logs "Request received" to the console, and hands on execution.
-app.use((req, res, next) => {
-    console.log('Request received!');
-    next();
-});
-// Adds a 201 status code to the response, and hands on execution.
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-})
-// Sends the JSON response, and hands on execution.
-app.use((req, res, next) => {
-    res.json({
-        message: 'Your request was successful!'
-    });
-    next();
-});
-// Logs "Response sent successfully" to the console.
-app.use((req, res, next) => {
-    console.log('Response sent successfully!');
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [{
+            _id: 'oainfeoifnaoin',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: '',
+            price: 4900,
+            userId: 'oaignoainge',
+        },
+        {
+            _id: 'aigoeaingoieng',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: '',
+            price: 2900,
+            userId: 'oaignoainge',
+        },
+
+    ];
+    res.status(200).json(stuff);
 });
 
 module.exports = app;
