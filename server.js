a3c7208e645bd62cb13849bc14e1e6584de54f4e
Joseph Joslin <jjoslin07@gmail.com>
2021-07-06 10:12:17 -0700

    Improve server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const errorHandler = error => {
         throw error;
     }
     const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe' + address : 'port: ' + port;
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
     switch (error.code) {
         case 'EACCES':
             console.error(bind + ' requires elevated privileges.');
