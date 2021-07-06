d4746d4bb79c5923650b59d51e9937516dffd03a
Joseph Joslin <jjoslin07@gmail.com>
2021-07-06 13:04:58 -0700

    Receive stuff from the front-end app

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use((req, res, next) => {
@@ -6,6 +8,15 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Thing created successfully!'
+    });
 });
 
 app.use('/api/stuff', (req, res, next) => {
