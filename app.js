2125e5644f15ad2d0e98baf281b377181bced345
Joseph Joslin <jjoslin07@gmail.com>
2021-07-07 14:13:46 -0700

    Set up authentication routes

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://Joe:ZCEoqhb8nF13Qg4l@cluster0.fip4x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
@@ -25,5 +28,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
