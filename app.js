306b8b25acc490f19bde2ae5c305d7848ffb6f52
Joseph Joslin <jjoslin07@gmail.com>
2021-07-07 15:46:32 -0700

    Modify the POST route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
@@ -27,7 +28,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+
 
 module.exports = app;
